d9123af8eab5c264bbe5e58ef514c231b3de5343
Todd Niswonger <tniswong@gmail.com>
2011-08-02 16:25:34 -0500

    Release 1.0

--- DomainSchemagenGrailsPlugin.groovy
+++ DomainSchemagenGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class DomainSchemagenGrailsPlugin {
     // the plugin version
-    def version = "0.1"
+    def version = "1.0"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
@@ -14,9 +14,7 @@ class DomainSchemagenGrailsPlugin {
     def author = "Todd Niswonger"
     def authorEmail = "tniswong@gmail.com"
     def title = "Domain Schemagen"
-    def description = '''\\
-Automatically generate an XML Schema from your domain classes.
-'''
+    def description = "Automatically generate an XML Schema from your domain classes."
 
     // URL to the plugin's documentation
     def documentation = "http://grails.org/plugin/domain-schemagen"
